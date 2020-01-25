89b79764cac4557dfa74bfbb2184aadd390ef2ed
houfio <26674115+houfio@users.noreply.github.com>
2020-01-26 00:23:34 +0100

    Add button sizes

--- components/button/button.stories.tsx
+++ components/button/button.stories.tsx
@@ -9,14 +9,52 @@ export default {
   title: 'Button'
 };
 
-export const withLabel = () => (
-  <Button text={text('Label', 'button')} onClick={action('click')}/>
+export const label = () => (
+  <>
+    <Button text={text('Label', 'button')} onClick={action('click')} size="small"/>
+    <br/>
+    <Button text={text('Label', 'button')} onClick={action('click')}/>
+    <br/>
+    <Button text={text('Label', 'button')} onClick={action('click')} size="big"/>
+  </>
 );
 
-export const withSpinner = () => (
-  <Button text={text('Label', 'button')} loading={true}/>
+export const loading = () => (
+  <>
+    <Button text="button" loading={true} size="small"/>
+    <br/>
+    <Button text="button" loading={true}/>
+    <br/>
+    <Button text="button" loading={true} size="big"/>
+  </>
 );
 
-export const withIcon = () => (
-  <Button text={text('Label', 'button')} onClick={action('click')} icon={faStroopwafel}/>
+export const icon = () => (
+  <>
+    <Button text={text('Label', 'button')} onClick={action('click')} icon={faStroopwafel} size="small"/>
+    <br/>
+    <Button text={text('Label', 'button')} onClick={action('click')} icon={faStroopwafel}/>
+    <br/>
+    <Button text={text('Label', 'button')} onClick={action('click')} icon={faStroopwafel} size="big"/>
+  </>
+);
+
+export const onlyIcon = () => (
+  <>
+    <Button text="button" onClick={action('click')} icon={faStroopwafel} iconOnly={true} size="small"/>
+    <br/>
+    <Button text="button" onClick={action('click')} icon={faStroopwafel} iconOnly={true}/>
+    <br/>
+    <Button text="button" onClick={action('click')} icon={faStroopwafel} iconOnly={true} size="big"/>
+  </>
+);
+
+export const disabled = () => (
+  <>
+    <Button text={text('Label', 'button')} disabled={true} size="small"/>
+    <br/>
+    <Button text={text('Label', 'button')} disabled={true}/>
+    <br/>
+    <Button text={text('Label', 'button')} disabled={true} size="big"/>
+  </>
 );
--- components/button/index.tsx
+++ components/button/index.tsx
@@ -28,29 +28,64 @@ type Props = {
   /**
    * Toggle disabled state
    */
-  disabled?: boolean
+  disabled?: boolean,
+  /**
+   * Size of spacing and text inside the button
+   */
+  size?: 'small' | 'normal' | 'big'
 };
 
-export function Button({ text, onClick, icon, iconOnly = false, loading = false, disabled = false }: Props) {
+export function Button({ text, onClick, icon, iconOnly = false, loading = false, disabled = false, size = 'normal' }: Props) {
   const StyledButton = useStyled('button')`
     padding: .5rem .75rem;
     color: white;
-    background-color: dodgerblue;
+    background-color: rgba(30, 144, 255, 1);
+    font-size: 1rem;
     border: none;
     border-radius: .5rem;
+    user-select: none;
     cursor: pointer;
-    transition: color .25s ease;
+    transition: color .25s ease, transform .25s ease, opacity .25s ease, box-shadow .25s ease;
     [loading="true"] {
       color: transparent;
+    }
+    [size="small"] {
+      padding: .25rem .5rem;
+      font-size: .75rem;
+    }
+    [size="big"] {
+      padding: .75rem 1rem;
+      font-size: 1.25rem;
+    }
+    :active {
+      transform: translateY(.1rem);
+    }
+    :disabled {
+      opacity: .5;
       cursor: default;
+    }
+    :focus {
+      outline: none;
+      box-shadow: 0 0 0 .25rem rgba(30, 144, 255, .5);
+    }
+  `;
+  const StyledIcon = useStyled(FontAwesomeIcon)`
+    [only="false"] {
+      margin-right: .75rem;
+      [size="small"] {
+        margin-right: .5rem;
+      }
+      [size="big"] {
+        margin-right: 1rem;
+      }
     }
   `;
 
   return (
-    <Spinner spinning={loading}>
-      <StyledButton onClick={onClick} tabIndex={loading ? -1 : 0} disabled={disabled} data-loading={loading} aria-label={text}>
+    <Spinner spinning={loading} size={size}>
+      <StyledButton onClick={onClick} disabled={disabled} data-loading={loading} data-size={size} aria-label={text}>
         {icon && (
-          <FontAwesomeIcon icon={icon}/>
+          <StyledIcon icon={icon} data-only={iconOnly} data-size={size}/>
         )}
         {(!icon || !iconOnly) && text}
       </StyledButton>
