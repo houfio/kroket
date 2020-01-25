4cbe1dde306c51170b5f9724befff1014ff4794c
houfio <26674115+houfio@users.noreply.github.com>
2020-01-25 20:25:10 +0100

    Add breakpoiint support to styled

--- utils/styled/index.ts
+++ utils/styled/index.ts
@@ -1,25 +1,24 @@
+import { Breakpoint, BREAKPOINTS } from '@kroket/breakpoint';
 import { css } from '@kroket/css';
 import { ClassAttributes, createElement, forwardRef, useMemo } from 'react';
 
 type ElementProps<T extends keyof JSX.IntrinsicElements> = JSX.IntrinsicElements[T] extends ClassAttributes<infer A> ? A : never;
 
 export function useStyled<T extends keyof JSX.IntrinsicElements>(element: T) {
-  return (literals: TemplateStringsArray, ...extra: unknown[]) => {
-    return useMemo(() => {
-      let style = '';
+  return (literals: TemplateStringsArray, ...breakpoints: Breakpoint[]) => {
+    let style = '';
 
-      for (let i = 0; i < extra.length; i++) {
-        style += literals[i];
-        style += extra[i];
-      }
+    for (let i = 0; i < breakpoints.length; i++) {
+      style += literals[i];
+      style += `@media (min-width: ${BREAKPOINTS[breakpoints[i]]}rem)`;
+    }
 
-      style += literals[literals.length - 1];
+    style += literals[literals.length - 1];
 
-      return forwardRef<ElementProps<T>, JSX.IntrinsicElements[T]>((props, ref) => createElement(element, {
-        ...props,
-        className: css(style),
-        ref
-      }));
-    }, [...literals, ...extra]);
+    return useMemo(() => forwardRef<ElementProps<T>, JSX.IntrinsicElements[T]>((props, ref) => createElement(element, {
+      ...props,
+      className: css(style),
+      ref
+    })), [style]);
   };
 }
