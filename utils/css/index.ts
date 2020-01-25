ef40987f3878ca4296a45c3bd4ac34e6cdf2ecff
houfio <26674115+houfio@users.noreply.github.com>
2020-01-25 20:24:40 +0100

    Improve css parser

--- utils/css/index.ts
+++ utils/css/index.ts
@@ -1,13 +1,42 @@
 import { hash } from '@kroket/hash';
 import { inject } from '@kroket/inject';
+import { memoize } from '@kroket/memoize';
 
 const stylis = require('stylis');
 
-export function css(style: string) {
+export const css = memoize((style: string) => {
   const selector = `kroket-${hash(style)}`;
   const result = stylis(`.${selector}`, style) as string;
+  let styles: string[] = [];
+  let current = '';
+  let depth = 0;
 
-  inject(result.split('}').filter(Boolean).map((rule) => `${rule}}`));
+  for (const character of result) {
+    if (character === '[') {
+      current = `${current.substr(0, current.length - 1)}[data-`;
+    } else {
+      current += character;
+    }
+
+    if (character === '{') {
+      depth++;
+    } else if (character === '}') {
+      depth--;
+
+      if (!depth) {
+        if (current) {
+          styles = [
+            ...styles,
+            current
+          ];
+        }
+
+        current = '';
+      }
+    }
+  }
+
+  inject(styles);
 
   return selector;
-}
+});
