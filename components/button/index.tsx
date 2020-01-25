554333cee9025fd1c079c42f925197e98f6f8540
houfio <26674115+houfio@users.noreply.github.com>
2020-01-25 16:10:29 +0100

    Add button props

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,3 +1,4 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { useStyled } from '@kroket/styled';
 import * as React from 'react';
 import { ReactNode } from 'react';
@@ -10,10 +11,26 @@ type Props = {
   /**
    * Event called on every button click
    */
-  onClick?: () => void
+  onClick?: () => void,
+  /**
+   * Icon displayed in the button
+   */
+  icon?: IconProp,
+  /**
+   * Only display icon
+   */
+  iconOnly?: boolean,
+  /**
+   * Toggle button loading state, automatically disables the button
+   */
+  loading?: boolean,
+  /**
+   * Toggle disabled state in button
+   */
+  disabled?: boolean
 };
 
-export function Button({ text, onClick }: Props) {
+export function Button({ text, onClick, icon, iconOnly = true, loading = false, disabled = false }: Props) {
   const Component = useStyled('button')`
     background-color: yellow;
   `;
