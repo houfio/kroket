db41d5a98e12cfbdfc236dbd8c4fce17b3053eb0
houfio <26674115+houfio@users.noreply.github.com>
2020-01-25 20:34:59 +0100

    Add memoize tests

---
+++ utils/memoize/memoize.tests.ts
@@ -0,0 +1,42 @@
+import { memoize } from '.';
+
+type ParseFn = (value: string) => number;
+
+let parse: ParseFn;
+let memoizedParse: ParseFn;
+
+beforeEach(() => {
+  parse = jest.fn().mockImplementation((value: string) => parseInt(value, 10));
+  memoizedParse = memoize(parse);
+});
+
+it('should return the result of a function', () => {
+  expect(memoizedParse('1')).toBe(1);
+});
+
+it('should return the same result if the arguments have not changed', () => {
+  expect(memoizedParse('1')).toBe(1);
+  expect(memoizedParse('1')).toBe(1);
+});
+
+it('should not execute the memoized function if the arguments have not changed', () => {
+  memoizedParse('1');
+  memoizedParse('1');
+
+  expect(parse).toHaveBeenCalledTimes(1);
+});
+
+it('should invalidate a memoize cache if new arguments are provided', () => {
+  expect(memoizedParse('1')).toBe(1);
+  expect(memoizedParse('2')).toBe(2);
+  expect(parse).toHaveBeenCalledTimes(2);
+});
+
+it('should resume memoization after a cache invalidation', () => {
+  expect(memoizedParse('1')).toBe(1);
+  expect(parse).toHaveBeenCalledTimes(1);
+  expect(memoizedParse('2')).toBe(2);
+  expect(parse).toHaveBeenCalledTimes(2);
+  expect(memoizedParse('2')).toBe(2);
+  expect(parse).toHaveBeenCalledTimes(2);
+});
