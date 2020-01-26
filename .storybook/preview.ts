effd1de04e2dae0acadb44f8cd5502371444fa63
houfio <26674115+houfio@users.noreply.github.com>
2020-01-26 15:44:08 +0100

    Disable color contrast check

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,20 @@
 import { withA11y } from '@storybook/addon-a11y';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
-import { addDecorator } from '@storybook/react';
+import { addDecorator, addParameters } from '@storybook/react';
 
 addDecorator(withA11y);
 addDecorator(withInfo);
 addDecorator(withKnobs);
+addParameters({
+  a11y: {
+    config: {
+      rules: [
+        {
+          id: 'color-contrast',
+          enabled: false
+        }
+      ]
+    }
+  }
+});
