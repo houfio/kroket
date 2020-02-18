2ad9efd8e347f2b40ee9b3863a3164200f35fc76
houfio <26674115+houfio@users.noreply.github.com>
2020-02-18 10:12:49 +0100

    Remove custom storybook styling

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -4,23 +4,6 @@ import { create } from '@storybook/theming';
 addons.setConfig({
   theme: create({
     base: 'light',
-    colorPrimary: 'black',
-    colorSecondary: 'black',
-    appBg: 'white',
-    appContentBg: 'white',
-    appBorderColor: 'transparent',
-    appBorderRadius: 8,
-    fontBase: '"Open Sans", sans-serif',
-    fontCode: 'monospace',
-    textColor: 'black',
-    textInverseColor: 'white',
-    barTextColor: 'black',
-    barSelectedColor: 'black',
-    barBg: 'rgba(0,0,0, .05)',
-    inputBg: 'rgba(0,0,0, .05)',
-    inputBorder: 'transparent',
-    inputTextColor: 'black',
-    inputBorderRadius: 8,
     brandTitle: 'kroket',
     brandUrl: 'https://github.com/houfio/kroket',
     brandImage: 'https://raw.githubusercontent.com/houfio/kroket/master/logo_small.svg?sanitize=true'
