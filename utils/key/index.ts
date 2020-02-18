44b4f859e601101b707f478fcc56895d32da7469
houfio <26674115+houfio@users.noreply.github.com>
2020-02-18 10:12:59 +0100

    Add key handler package

---
+++ utils/key/index.ts
@@ -0,0 +1,21 @@
+import { DependencyList, useCallback, useEffect } from 'react';
+
+export function useKey(key: string, fn: () => void, deps: DependencyList) {
+  const callback = useCallback(fn, deps);
+
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      if (event.key !== key) {
+        return;
+      }
+
+      callback();
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [key, callback]);
+}
