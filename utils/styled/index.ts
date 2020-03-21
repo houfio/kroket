eff18181e6abf08c590d46f5c3a86e9e1d58deb8
houfio <26674115+houfio@users.noreply.github.com>
2020-03-21 15:27:00 +0100

    Add fn support to styled

--- utils/styled/index.ts
+++ utils/styled/index.ts
@@ -3,14 +3,16 @@ import { Theme, useTheme } from '@kroket/theme';
 import { ComponentPropsWithoutRef, createElement, ElementType, forwardRef, useMemo } from 'react';
 
 export function useStyled<T extends ElementType>(element: T) {
-  return (literals: TemplateStringsArray, ...colors: (keyof Theme)[]) => {
+  return (literals: TemplateStringsArray, ...colors: (keyof Theme | ((theme: Theme) => string))[]) => {
     const theme = useTheme();
 
     let style = '';
 
     for (let i = 0; i < colors.length; i++) {
       style += literals[i];
-      style += theme[colors[i]];
+
+      const fn = colors[i];
+      style += typeof fn === 'function' ? fn(theme) : theme[fn];
     }
 
     style += literals[literals.length - 1];
